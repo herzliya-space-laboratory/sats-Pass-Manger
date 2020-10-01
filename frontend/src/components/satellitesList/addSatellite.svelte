e213a85d438fb497bbac6910698b41abb48ead45
itai lupo <itailupo@gmail.com>
2020-10-01 10:50:39 +0300

    satllite page refactor + add satllite

---
+++ frontend/src/components/satellitesList/addSatellite.svelte
@@ -0,0 +1,54 @@
+<script>
+    
+    import axios from "axios";
+    
+    let name = '';
+    let satId = '';
+    
+    function sendSatllliteToDataBase(){
+        axios.post("http://localhost:5000/api/v1/satellite/", {name, satId})
+    }
+</script>
+
+<div class='box'>
+    <form on:submit={sendSatllliteToDataBase}>
+        <input bind:value={name} placeholder="satlllite name" />
+        <input bind:value={satId} placeholder="norad id" />
+        <input type='submit' value='create'/>
+    </form>
+</div>
+
+<style>
+    input{
+        padding: 12px 20px;
+        margin: 8px 0;
+        box-sizing: border-box;
+        border: 1px solid black;
+        border-radius: 4px;
+        width: 40%;
+    }
+
+    input[type="submit"]{
+        background-color: blue; /* Green */
+        border: none;
+        color: white;
+        padding: 12px 32px;
+        text-align: center;
+        display: inline-block;
+        font-size: 1em;
+        margin: 4px 2px;
+        cursor: pointer;
+        width: 16%;
+    }
+
+    input[type="submit"]:hover {
+        background-color: white; 
+        color: black; 
+    }
+
+    .box{
+        border: 1px solid #333;
+        padding: 10px;
+        margin: 0;
+    }
+</style>
--- frontend/src/routes/satellitesList/index.svelte
+++ frontend/src/routes/satellitesList/index.svelte
@@ -1,29 +1,19 @@
 
 <script context="module">
-	import Satellite from './Satellite.svelte'
-
-	import axios from "axios";
-	
+    import axios from "axios";
+    
     export async function preload(page, session) {
         let res = await axios.get("http://localhost:5000/api/v1/satellite/");
         const data = res.data.data;
         return { data };
 	}
-	
 </script>
 
 <script>
-    let satname = '';
-    let satid = '';
-    let f = '';
+    import Satellite from '../../components/satellitesList/Satellite.svelte';
+    import AddSatellite from '../../components/satellitesList/addSatellite.svelte';
+    
     export let data;
-    function onPress(satid, satname){
-        if(satid == f || satname == f){
-            alert(`u need to put something in there`);
-        }
-        
-
-    }
 </script>
 
 
@@ -32,57 +22,22 @@
 </svelte:head>
 
 
-<input bind:value={satname} placeholder="sat name">
-<input bind:value={satid} placeholder="sat id">
-<button on:click= {onPress}>add sat </button>
+
 
 <div class = 'box'>
 	{#each data as satellite}
 		<Satellite {...satellite}/>
-		
 	{/each}
 </div>
 
+<AddSatellite />
+
 <style>
-    button{
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 16px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 14px;
-  margin: 4px 2px;
-  transition-duration: 0.4s;
-  cursor: pointer;
-  width: 16%;
-}
-
-button:hover {
-  background-color: white; 
-  color: black; 
-  border: 2px solid #4CAF50;
-}
-
-
-
-    
-    input{
-        padding: 12px 20px;
-        margin: 8px 0;
-        box-sizing: border-box;
-        border: 2px solid red;
-        border-radius: 4px;
-        width: 40%;
-    }
-	
-
-	.box{
-        border: 2px solid #333;
+   	.box{
+        border: 1px solid #333;
         padding: 0px;
-        margin: 10px 5px 5px 0px;
-        height: 800px;
+        margin: 0;
+        height: 600px;
         overflow: auto;
     }
 </style>
