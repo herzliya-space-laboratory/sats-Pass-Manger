f5bc3472f3b1592677e55f45e5f47b98267d95d0
royrab <royrab23@gmail.com>
2020-10-01 11:15:50 +0300

    css

--- frontend/src/routes/satellitesList/index.svelte
+++ frontend/src/routes/satellitesList/index.svelte
@@ -10,10 +10,19 @@
 </script>
 
 <script>
+<<<<<<< Updated upstream
 	import Satellite from '../../components/satellitesList/Satellite.svelte';
 	import AddSatellite from '../../components/satellitesList/addSatllite.svelte';
 
     export let data;
+=======
+    var name = '';
+    var satId = '';
+    export let data;
+    function onSubmit(){
+        axios.post(`http://localhost:5000/api/v1/satellite`, {satId, name});
+    }
+>>>>>>> Stashed changes
 </script>
 
 
@@ -21,6 +30,16 @@
 	<title> satellites list </title>
 </svelte:head>
 
+<<<<<<< Updated upstream
+=======
+
+<form on:click= {onSubmit}>
+    <input type="text" bind:value={name} placeholder="sat name">
+    <input type="text" bind:value={satId} placeholder="sat id">
+    <input type= "submit" value="create">
+</form>
+
+>>>>>>> Stashed changes
 <div class = 'box'>
     {#each data as satellite}
         <Satellite {...satellite}/>
@@ -28,6 +47,30 @@
 </div>
 <AddSatellite />
 
+<<<<<<< Updated upstream
+=======
+<style>
+    input[type="submit"]{
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  width: 16%;
+}
+
+input[type="submit"]:hover {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #4CAF50;
+}
+>>>>>>> Stashed changes
 
 
 <style>
